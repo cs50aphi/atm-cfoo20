4161e3d123a2bf8f10791280cb8f044cd128350b
cfoo20 <cfoo20@punahou.edu>
2020-04-06 06:27:06 +0000

    ATM pseudocode

--- ATM.java
+++ ATM.java
@@ -4,6 +4,45 @@ public class ATM
 {
     public static void main(String[] args)
     {
-
+        // Initiate Scanner
+        Scanner kb = new Scanner(System.in);
+        // Create new accounts
+        CheckingAccount checking = new CheckingAccount(15);
+        // Greet user
+        // Set quit to false
+        // count # of transactions
+        // repeat until user enters q or Q
+        // present menu of options
+        // if deposit
+            // ask which account
+            // ask how much user wants to deposit
+            // add amount to account
+            // print balance of both accounts
+            // add 1 to number of transactions
+        // if withdraw
+            // ask which account
+            // ask how much user wants to withdraw
+            // subtract ammount from account
+            // print balance of both accounts
+            // add 1 to number of transactions
+        // if check balance
+            // print balance of both accounts
+            // add 1 to number of transactions
+        // if transfer
+            // ask checking to savings or vice versa
+            // ask how much
+            // if checking to savings
+                // withdraw user amount from checking
+                // deposit user amount to savings
+            // if savings to checking
+                // withdraw user amount from savings
+                // deposit user amount to checkings
+            // print balance of both accounts
+            // add 1 to number of transactions
+        // if # of transactions is a multiple of 5
+            // calculate interest
+            // print balance of both accounts
+        // if quit
+            // set quit to true
     }
 }
